a3208208ce89d53115136cc4a7cc424c915349b1
Shrihari Mane <138723848+shriharimane@users.noreply.github.com>
2026-02-20 20:56:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      letterSpacing: {
+        ultra: "0.6em"
+      }
+    }
+  },
+  plugins: []
+};
